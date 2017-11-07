0e5ca4bb9b24f09924fd7230a6f94dce36cfdaab
Hewlette Dibble <hcodydibble@gmail.com>
2017-11-06 20:47:03 -0800

    get_inspection_page and load_inspection_page functions working

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# basic-scraped
+# King County Data Scraper
+
+When this app is complete I should be able to scrape data health inspection data from King County.
+
+**Libraries**
+Using [Requests](http://docs.python-requests.org/en/master/) to make the GET requests to the King County API.
---
+++ scraper.py
@@ -0,0 +1,37 @@
+"""A little baby data scrapper."""
+import requests
+
+
+URL = "http://info.kingcounty.gov/health/ehs/foodsafety/inspections/Results.aspx"
+PAYLOAD = {"Business_Name": "",
+           "Business_Address": "",
+           "Longitude": "",
+           "Latitude": "",
+           "City": "",
+           "Zip_Code": "",
+           "Inspection_Type": "All",
+           "Inspection_Start": "",
+           "Inspection_End": "",
+           "Inspection_Closed_Business": "A",
+           "Violation_Points": "",
+           "Violation_Red_Points": "",
+           "Violation_Descr": "",
+           "Fuzzy_Search": "N",
+           "Sort": "B"}
+
+
+def get_inspection_page(**kwargs):
+    """."""
+    new_params = PAYLOAD.copy()
+    new_params.update(kwargs)
+    response = requests.get(URL, params=new_params)
+    response.raise_for_status()
+    return response.content, response.encoding
+
+
+def load_inspection_page():
+    """."""
+    with open('inspection_page.html') as f:
+        content = f.read().encode('utf8')
+        encoding = f.read()
+        return content, encoding
