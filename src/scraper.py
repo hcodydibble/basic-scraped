f5392399b4385ce0a10794887079d7d89056602d
Hewlette Dibble <hcodydibble@gmail.com>
2017-11-07 13:39:56 -0800

    changing stuff

--- src/scraper.py
+++ src/scraper.py
@@ -1,5 +1,6 @@
 """A little baby data scrapper."""
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 
@@ -22,7 +23,12 @@ PAYLOAD = {"Business_Name": "",
 
 
 def get_inspection_page(**kwargs):
-    """."""
+    """Send GET request to King County Health Inspection API.
+
+    The request will use the kwargs to fill out the requests parameters as the
+    user specifies and fill in any unspecified ones with any required default
+    values.
+    """
     new_params = PAYLOAD.copy()
     new_params.update(kwargs)
     response = requests.get(URL, params=new_params)
@@ -31,13 +37,32 @@ def get_inspection_page(**kwargs):
 
 
 def load_inspection_page():
-    """."""
+    """Open the inspection_page.html and read it.
+
+    The purpose of this function is to keep the user from having to make
+    repeated requests at the KCHI API which has a tendency to not like
+    the attention. It will return a bytes version and a string version
+    of the HTML.
+    """
     with open('inspection_page.html') as f:
         content = f.read().encode('utf8')
-        encoding = f.read()
+        encoding = 'utf8'
         return content, encoding
 
 
-def parse_source(html):
+def parse_source(content, encoding='utf8'):
     """."""
-    return BeautifulSoup(html, "lxml")
+    parsed = BeautifulSoup(content, "lxml", from_encoding=encoding)
+    return parsed
+
+
+# if __name__ == "__main__":
+#     kwargs = {
+#         "Zip_Code": "98116",
+#         "City": "Seattle"
+#     }
+#     if len(sys.argv) > 1 and sys.argv[1] == "test":
+#         content, encoding = load_inspection_page()
+#     else:
+#         content, encoding = get_inspection_page(**kwargs)
+#     doc = parse_source(content)
